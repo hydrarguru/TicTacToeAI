56417dc43b3c0047490273c589090a2a4841da04
hydrarguru <henrikengqvist99@gmail.com>
2019-11-25 14:50:11 +0100

    initial commit

---
+++ Assets/Scripts/AI.cs
@@ -0,0 +1,213 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class AI : MonoBehaviour
+{
+	public struct Move
+	{
+		public int row, col;
+	}
+
+	[SerializeField] private int maxDepth = 3;
+	private string player = "X";
+	private string computer = "O";
+	private GameController controller;
+	private int rows;
+	private int columns;
+	private const int MIN = -1000;
+	private const int MAX = 1000;
+
+	private void Awake()
+	{
+		controller = GetComponentInParent<GameController>();
+		rows = controller.rows;
+		columns = controller.columns;
+	}
+	
+	/*MovesLeft:
+	 self explanatory
+    */
+	bool MovesLeft(Text[,] board)
+	{
+		for (int i = 0; i < rows; i++)
+		for (int j = 0; j < columns; j++)
+			if (board[i, j].text == "")
+				return true;
+		return false;
+	}
+
+	/*
+	 * Evaluate:
+	 * Returns a value based on who is winning 
+	 */
+	int Evaluate(Text[,] board)
+	{
+		int computerMoves = 0;
+		int playerMoves = 0;
+
+		for (int row = 0; row < rows; row++)
+		{
+
+			playerMoves = 0;
+			computerMoves = 0;
+
+			for (int col = 0; col < columns; col++)
+			{
+				if (board[row, col].text == computer)
+					computerMoves++;
+				if (board[row, col].text == player)
+					playerMoves++;
+			}
+
+			if (computerMoves == columns)
+				return 10;
+			if (playerMoves == columns)
+				return -10;
+		}
+
+		for (int col = 0; col < columns; col++) 
+		{
+        			
+        			playerMoves = 0;
+        			computerMoves = 0;
+
+                    for (int row = 0; row < rows; row++) {
+        				if (board[row, col].text == computer)
+        					computerMoves++;
+        				if (board[row, col].text == player)
+        					playerMoves++;
+        			}
+         
+        			if (computerMoves == rows)
+        				return 10;
+        			if (playerMoves == rows)
+        				return -10;
+		}
+		
+		playerMoves = 0;
+		computerMoves = 0;
+		for (int row = 0; row < rows; row++) {
+			if (board[row, row].text == computer)
+				computerMoves++;
+			if (board[row, row].text == player)
+				playerMoves++;
+
+			if (computerMoves == rows)
+				return 10;
+			if (playerMoves == rows)
+				return -10;
+		}
+		
+		playerMoves = 0;
+		computerMoves = 0;
+		for (int row = 0; row < rows; row++) {
+			if (board[row, (rows - 1) - row].text == computer)
+				computerMoves++;
+			if (board[row, (rows - 1) - row].text == player)
+				playerMoves++;
+
+			if (computerMoves == rows)
+				return 10;
+			if (playerMoves == rows)
+				return -10;
+		}
+		return 0;	
+	}
+
+	/*
+	 * MinMax
+	 * This is the minimax function. It considers all
+	 * the possible ways the game can go and returns 
+	 */
+	int MinMax(Text[,] board, int depth, bool isMax)
+	{
+		int score = Evaluate(board);
+		
+		if (depth == maxDepth)
+			return score;
+
+		if (score == 10)
+			return score;
+		
+		if (score == -10)
+			return score;
+
+		//Draw
+		if (MovesLeft(board) == false)
+			return 0;
+
+		if (isMax) 
+		{
+			int best = MIN;
+
+			for (int i = 0; i < rows; i++) {
+				for (int j = 0; j < columns; j++) {
+					if (board[i, j].text == "") {
+						board[i, j].text = computer;
+						int value = Math.Max(best, MinMax(board, depth + 1, !isMax));
+						best = Math.Max(best, value);
+
+						board[i, j].text = "";
+					}
+				}
+			}
+			return best;
+		}
+		else 
+		{
+			int best = MAX;
+
+			for (int i = 0; i < rows; i++) {
+				for (int j = 0; j < columns; j++) {
+					if (board[i, j].text == "") {
+						board[i, j].text = player;
+						int value = Math.Min(best, MinMax(board, depth + 1, !isMax));
+						best = Math.Min(best, value);
+						
+						board[i, j].text = "";
+						
+					}
+				}
+			}
+			return best;
+		}
+	}
+	
+	
+	/*
+	 * BestMove:
+	 * evaluate minimax function for
+	 * all empty cells.
+	 */
+	public Move BestMove(Text[,] board)
+	{
+		int bestVal = MIN;// -1000
+		Move bestMove;
+		bestMove.row = -1;
+		bestMove.col = -1;
+
+		for (int i = 0; i < rows; i++)
+		{
+			for (int j = 0; j < columns; j++)
+			{
+				if (board[i, j].text == "")
+				{
+					board[i, j].text = computer;
+					int moveValue = MinMax(board, 0, false);
+					board[i, j].text = "";
+
+					if (moveValue > bestVal)
+					{
+						bestMove.row = i;
+						bestMove.col = j;
+						bestVal = moveValue;
+					}
+				}
+			}
+		}
+		return bestMove;
+	}
+}
---
+++ Assets/Scripts/GameController.cs
@@ -0,0 +1,207 @@
+﻿using UnityEngine;
+using UnityEngine.UI;
+using System.Collections;
+
+public class GameController : MonoBehaviour {
+    
+   public Text[,] buttonListArray = new Text[3, 3];
+   public Text[] buttonList;
+   public GameObject gameOverPanel;
+   public Text gameOverText;
+   public GameObject restartButton;
+   public bool playAgainstAI = true;
+   //Board
+   public int rows = 3;
+   public int columns = 3;
+   
+   private int moveCount;
+   private string playerSide;
+   private string computerSide;
+
+   private AI computerAI;
+   private bool playerTurn;
+   
+   
+
+   void Awake()
+   {
+       buttonListArray = CreateBoard(buttonList, rows, columns);
+       SetGameControllerReferenceOnButtons();
+       playerSide = "X";
+       computerSide = "O";
+       gameOverPanel.SetActive(false);
+       moveCount = 0;
+       restartButton.SetActive(false);
+       computerAI = GetComponent<AI>();
+       playerTurn = true;
+   }
+
+   void SetGameControllerReferenceOnButtons()
+   {
+       for (int i = 0; i < rows; i++)
+       {
+           for (int j = 0; j < columns; j++)
+           {
+               buttonListArray[i, j].GetComponentInParent<GridSpace>().SetGameControllerReference(this);
+           }
+       }
+       
+       
+       /*
+       for (int i = 0; i < buttonList.Length; i++)
+       {
+           buttonList[i].GetComponentInParent<GridSpace>().SetGameControllerReference(this);
+       }
+       */
+   }
+
+   public string GetPlayerSide()
+   {
+       return playerSide;
+   }
+
+   public void EndTurn()
+   {
+       moveCount++;
+       if (GetWinState(playerSide))
+           GameOver(playerSide);
+       else if (GetWinState(computerSide))
+           GameOver(computerSide);
+       else if (moveCount >= rows*columns)
+           GameOver("draw");
+       else
+           ChangeSides();
+   }
+
+   void ChangeSides()
+   {
+       //Choose between playing against AI or a player
+       if (!playAgainstAI)
+           playerSide = (playerSide == "X") ? "O" : "X";
+       if (playAgainstAI)
+       {
+           playerTurn = !playerTurn;
+           if (!playerTurn)
+               ComputerAITurn();
+       }
+   }
+
+   void GameOver(string winningPlayer)
+   {
+       SetBoardInteractable(false);
+       if (winningPlayer == "draw")
+       {
+           SetGameOverText("It's a Draw!");
+       } else
+       {
+           SetGameOverText(winningPlayer + " Wins!");
+       }
+       restartButton.SetActive(true);
+   }
+
+   void SetGameOverText(string value)
+   {
+       gameOverPanel.SetActive(true);
+       gameOverText.text = value;
+   }
+
+   public void RestartGame()
+   {
+       playerSide = "X";
+       moveCount = 0;
+       gameOverPanel.SetActive(false);
+       restartButton.SetActive(false);
+       playerTurn = true;
+       
+       for (int i = 0; i < rows; i++) {
+           for (int j = 0; j < columns; j++) {
+               buttonListArray[i, j].text = "";
+           }
+       }
+       
+       SetBoardInteractable(true);
+   }
+
+   void SetBoardInteractable(bool toggle)
+   {
+       for (int i = 0; i < rows; i++)
+       {
+           for (int j = 0; j < columns; j++)
+           {
+               buttonListArray[i, j].GetComponentInParent<Button>().interactable = toggle;
+           }
+       }
+       /*
+       for (int i = 0; i < buttonList.Length; i++)
+       {
+           buttonList[i].GetComponentInParent<Button>().interactable = toggle;
+       }
+       */
+   }
+
+   void ComputerAITurn()
+   {
+       AI.Move computerMove = computerAI.BestMove(buttonListArray);
+       buttonListArray[computerMove.row, computerMove.col].text = computerSide;
+       buttonListArray[computerMove.row, computerMove.col].GetComponentInParent<Button>().interactable = false;
+       
+       
+       EndTurn();
+   }
+
+   private bool GetWinState(string side)
+   {
+       int counter = 0;
+       for (int row = 0; row < rows; row++) {
+           counter = 0;
+           for (int col = 0; col < columns; col++) {
+               if (buttonListArray[row, col].text == side)
+                   counter++;
+           }
+
+           if (counter == rows)
+               return true;
+       }
+
+       for (int col = 0; col < columns; col++) {
+           counter = 0;
+           for (int row = 0; row < rows; row++) {
+               if (buttonListArray[row, col].text == side)
+                   counter++;
+           }
+
+           if (counter == columns)
+               return true;
+       }
+
+       counter = 0;
+       for (int row = 0; row < rows; row++) {
+           if (buttonListArray[row, row].text == side)
+               counter++;
+           if (counter == rows)
+               return true;
+       }
+
+       counter = 0;
+       for (int row = 0; row < rows; row++) {
+           if (buttonListArray[row, (rows - 1) - row].text == side)
+               counter++;
+           if (counter == rows)
+               return true;
+       }
+       return false;
+   }
+   
+   
+   private Text[,] CreateBoard(Text[] board, int rows, int columns) {
+       Text[,] output = new Text[rows, columns];
+       for (int i = 0; i < rows; i++) {
+           for (int j = 0; j < columns; j++) {
+               output[i, j] = board[i * columns + j];
+           }
+       }
+       return output;
+   }
+}
+
+
